fe758ef9d2f78a424a031bdd31e3f725e5900ce7
James Brooks <jhbrooks@uchicago.edu>
2016-02-27 16:46:14 -0500

    Add User model with validations

---
+++ test/models/user_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(username: "Example", email: "ex@example.com",
+                     password: "3xamp13")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "username should be present" do
+    @user.username = "   "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "   "
+    assert_not @user.valid?
+  end
+
+  test "password should be present" do
+    @user.password = "   "
+    assert_not @user.valid?
+  end
+end
