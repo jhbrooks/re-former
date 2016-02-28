a5d920c83e9b883530ad55f0c1d96cb5e2cabbfd
James Brooks <jhbrooks@uchicago.edu>
2016-02-27 22:06:50 -0500

    Add users edit/update capabilities

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -13,4 +13,16 @@ class UsersControllerTest < ActionController::TestCase
                           password: "3x4mp13" }
     assert_response :redirect
   end
+
+  test "should get edit" do
+    get :edit, id: users(:one)
+    assert_response :success
+  end
+
+  test "should put update" do
+    put :update, id: users(:one), user: { username: "Example",
+                                                email: "ex@example.com",
+                                                password: "3x4mp13" }
+    assert_response :redirect
+  end
 end
